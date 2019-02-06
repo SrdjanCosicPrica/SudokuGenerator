4f0e16ebab300105fd6be97d0f5a647d0ee497ad
Srdjan Cosic <sadde.95@hotmail.com>
2019-02-06 07:04:44 +0100

    Add randomize command info to help command

--- src/com/srdjancosic/commands/HelpCommand.java
+++ src/com/srdjancosic/commands/HelpCommand.java
@@ -6,7 +6,8 @@ public class HelpCommand implements Command {
     @Override
     public void call(SudokuGenerator sudokuGenerator) {
         System.out.println("" +
-            "quit: Quit the program"
+            "quit: Quit the program" + System.lineSeparator() +
+            "randomize: Randomize a new Sudoku board"
         );
     }
 }
